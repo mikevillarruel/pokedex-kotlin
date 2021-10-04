3377782b8adb60176249926346dfc1c906d6dec7
Michael Villarruel <alejovillarruel@gmail.com>
2021-10-04 15:43:44 -0500

    set card color using Palette API

--- app/src/main/java/com/example/pokedex/ui/home/adapter/PokemonAdapter.kt
+++ app/src/main/java/com/example/pokedex/ui/home/adapter/PokemonAdapter.kt
@@ -1,10 +1,15 @@
 package com.example.pokedex.ui.home.adapter
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.palette.graphics.Palette
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.pokedex.data.model.Pokemon
 import com.example.pokedex.databinding.PokemonItemBinding
 
@@ -20,13 +25,29 @@ class PokemonAdapter(private val pokemonList: List<Pokemon>) :
         fun bind(item: Pokemon) {
             val urlSplit = item.url.split("/")
             val id = urlSplit[urlSplit.size - 2]
-
-            Glide.with(context)
-                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png")
-                .into(itemBinding.imgPokemon)
-
             itemBinding.txtPokemonName.text = item.name
             itemBinding.txtPokemonNumber.text = "#$id"
+
+            Glide.with(context)
+                .asBitmap()
+                .load("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png")
+                .into(object : CustomTarget<Bitmap>() {
+                    override fun onResourceReady(
+                        resource: Bitmap,
+                        transition: Transition<in Bitmap>?
+                    ) {
+                        val palette = Palette.from(resource).generate()
+                        itemBinding.constraintCard.setBackgroundColor(palette.getDominantColor(0))
+                        itemBinding.imgPokemon.setImageBitmap(resource)
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        // this is called when imageView is cleared on lifecycle call or for
+                        // some other reason.
+                        // if you are referencing the bitmap somewhere else too other than this imageView
+                        // clear it here as you can no longer have the bitmap
+                    }
+                })
         }
     }
 
